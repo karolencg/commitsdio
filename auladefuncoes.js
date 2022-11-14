82ead64a94046c1b8e4335509316da431c06cb65
karol <karolencg@gmail.com>
2022-11-14 16:24:25 -0300

    aula de funções

---
+++ auladefuncoes.js
@@ -0,0 +1,31 @@
+
+
+function calcularImc(peso, altura){
+    return peso / Math.pow(altura,2);
+}
+
+function classificarImc(imc){
+    if (imc < 18.5){
+        return'Abaixo do Peso';
+    } else if (imc >= 18.5 && imc <25){
+        return'Peso Normal';
+    }else if (imc > 25 && imc < 30){
+        return'Acima do Peso';
+    } else if (imc > 30 && imc < 40){
+        return'Obeso';
+    } else {
+        return'Obesidade Grave';
+    }
+}
+
+//main
+(function () {
+    const peso = 65;
+    const altura = 1.70;
+
+    const imc = calcularImc(peso, altura);
+    console.log(classificarImc(imc));
+
+})();
+
+
